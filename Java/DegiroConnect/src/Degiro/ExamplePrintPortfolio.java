a74e41dbb81efbe64096195b9f66e2e846831bee
degiroapi <deghero@yahoo.com>
2013-10-18 13:09:30 +0200

    print workdir at startup

--- Java/DegiroConnect/src/Degiro/ExamplePrintPortfolio.java
+++ Java/DegiroConnect/src/Degiro/ExamplePrintPortfolio.java
@@ -1,5 +1,7 @@
 package Degiro;
 
+import java.io.IOException;
+
 /**
  * Print portfolio at Degiro
  */
@@ -7,6 +9,13 @@ public class ExamplePrintPortfolio {
 
     public static void main(String [ ] args)
     {
+        try {
+            String current = new java.io.File( "." ).getCanonicalPath();
+            System.out.println("Current dir:"+current);
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
         PropertiesReader credentials = new PropertiesReader();
         credentials.read(DegiroConnect.USER_CREDENTIALS_FILENAME_DEFAULT);
         DegiroConnect degiro = new DegiroConnect(credentials);
